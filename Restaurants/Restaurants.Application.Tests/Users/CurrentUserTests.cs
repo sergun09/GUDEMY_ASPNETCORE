c30c4bea754ee636b97414b42f66ebaca5d90ec1
Sinan Ergun <ergun.sinan31@gmail.com>
2025-01-28 20:29:11 +0100

    Created test project

---
+++ Restaurants/Restaurants.Application.Tests/Users/CurrentUserTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Restaurants.Application.Users.Tests
+{
+    public class CurrentUserTests
+    {
+        [Fact()]
+        public void IsInRoleTest()
+        {
+
+        }
+    }
+}
